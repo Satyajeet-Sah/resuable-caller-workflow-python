298452c9031ad595f5c39925cfcc61b40e5c654c
Satyajeet <36815243+Satyajeet-Sah@users.noreply.github.com>
2026-09-22 10:45:40 +0530

    Initialize Flask app with health and users endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,20 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy",
+        "service": "reusable-workflow-python-api"
+    })
+
+@app.route("/api/users")
+def users():
+    return jsonify([
+        {"id": 1, "name": "David"},
+        {"id": 2, "name": "Micheal"}
+    ])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
